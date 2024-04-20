100316995c255292da97636f29397680bca20398
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-04-19 21:53:34 -0300

    first scripts

---
+++ cypress/e2e/cadastro_correto.cy.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://desafio.qa.bridge.ufsc.br/')
+    cy.get('[id="usuario"]').type('gustavoberettag@gmail.com')
+    cy.get('[id="password"]').type('PoP3PqEo2PzUAgOOkIqxEF')
+    cy.get('[id="termos-de-uso"]').click()
+    cy.contains('button','Acessar').click()
+  })
+})
